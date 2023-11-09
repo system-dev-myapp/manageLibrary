626b2609d017aaf85a84ec722c6ef6f43108db0a
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2023-11-09 22:50:13 +0700

    update

--- src/routers/RouterComponent/RouterComponent.1.jsx
+++ src/routers/RouterComponent/RouterComponent.1.jsx
@@ -1,14 +1,14 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import Home from "../../systems/pages/home/home";
-// import { RouterDTO } from "../../utils/routers.dto";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../systems/pages/home/home";
+import { RouterDTO } from "../../utils/routers.dto";
 
-// export default function RouterComponent() {
-//     return (
-//         <>
-//             <Routes>
-//                 <Route path={RouterDTO.dashboard} element={<Home />} />
-//             </Routes>
-//         </>
-//     );
-// }
+export default function RouterComponent() {
+    return (
+        <>
+            <Routes>
+                <Route path={RouterDTO.dashboard} element={<Home />} />
+            </Routes>
+        </>
+    );
+}
