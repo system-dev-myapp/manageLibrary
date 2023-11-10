961ad6a2fd25ec25e5ace8c3edd8dbefbb06ac78
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2023-11-10 10:20:37 +0700

    up

--- src/utils/routers.dto.js
+++ src/utils/routers.dto.js
@@ -3,6 +3,7 @@ export const RouterDTO = Object.freeze({
     auth: "/auth/:method",
     book: "/create-books",
     cate: {
+        manageCate: "/cate/*",
         createCate: "/cate/createCategory",
         allCate: "/cate/allCategory",
     },
