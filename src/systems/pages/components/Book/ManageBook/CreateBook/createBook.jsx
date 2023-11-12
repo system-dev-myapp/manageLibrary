b105e54f3c9c87b6ce557d6e274904a59fb19ee3
devtruongson <truongsonpt.80@gmail.com>
2023-11-12 17:26:53 +0700

    update

--- src/systems/pages/components/Book/ManageBook/CreateBook/createBook.jsx
+++ src/systems/pages/components/Book/ManageBook/CreateBook/createBook.jsx
@@ -40,7 +40,6 @@ export default function CreateBook() {
     ];
 
     const mdParser = new MarkdownIt(/* Markdown-it options */);
-
     async function handleSubmid() {
         if (!title || !cate || !active || !number || !markdown) {
             alert("Vui lòng điền đầy đủ thông tin");
